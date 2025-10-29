ecf71630a9d537491a3c8e28bfc750ceda87ea52
Priyanshu Joshi <joshipriyanshu575@gmail.com>
2025-10-29 08:39:33 +0530

    improvement

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json())
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-  origin:[ 'http://localhost:5173', "https://lustrous-donut-e71291.netlify.app/dashboard"],
+  origin:[ 'http://localhost:5173', "https://lustrous-donut-e71291.netlify.app"],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS' , 'PATCH'] ,
  allowedHeaders: ['Content-Type', 'Authorization', 'Origin', 'X-Requested-With', 'Accept']
 }));
